f58494c380404c1e4de841dda4804d311bcfef58
Bilalzaman <billalzaman8@gmail.com>
2018-11-09 17:18:27 +0500

    design fixing commit

--- AdvanceCallVideoRecorder/app/src/main/java/com/bilalzaman/advancecallvideorecorder/activities/HomeActivity.java
+++ AdvanceCallVideoRecorder/app/src/main/java/com/bilalzaman/advancecallvideorecorder/activities/HomeActivity.java
@@ -1,33 +1,25 @@
 package com.bilalzaman.advancecallvideorecorder.activities;
 
-import android.Manifest;
-import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bilalzaman.advancecallvideorecorder.R;
-import com.bilalzaman.advancecallvideorecorder.constants.Constants;
 import com.bilalzaman.advancecallvideorecorder.fragments.FavouriteFragment;
 import com.bilalzaman.advancecallvideorecorder.fragments.InboxFragment;
 import com.bilalzaman.advancecallvideorecorder.fragments.SettingsFragment;
 import com.bilalzaman.advancecallvideorecorder.fragments.TrashFragment;
 import com.bilalzaman.advancecallvideorecorder.helpers.BottomNavigationViewHelper;
 import com.bilalzaman.advancecallvideorecorder.helpers.PreferenceHelper;
-import com.bilalzaman.advancecallvideorecorder.helpers.UIHelper;
-
-import java.io.IOException;
 
 import static android.Manifest.permission.RECORD_AUDIO;
 import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
@@ -36,8 +28,6 @@ public class HomeActivity extends AppCompatActivity {
 
     private static int mLastNavigationSelectedItem;
     private BottomNavigationView mBottomNavigationView;
-    private TextView mTextMessage;
-    private static final int REQUEST_WRITE_PERMISSION = 1;
     public static final int RequestPermissionCode = 1;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -96,18 +86,12 @@ public class HomeActivity extends AppCompatActivity {
     }
 
     private void init() {
-//
-//        if (PreferenceHelper.getInstance().getString(Constants.ISVARIFIED, "").equalsIgnoreCase(Constants.ISVARIFIED)) {
-//            checkPermission();
-//            loadHomeFragment();
+
         if (checkPermission()) {
             loadHomeFragment();
         } else {
             requestPermission();
-           // permissionProceed();
-
         }
-
     }
 
     private void requestPermission() {
@@ -139,52 +123,4 @@ public class HomeActivity extends AppCompatActivity {
         int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
         return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
     }
-
-//    public void permissionProceed() {
-//        if (ContextCompat.checkSelfPermission(HomeActivity.this,
-//                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
-//            loadHomeFragment();
-//
-//        } else {
-//            requestStorePermission();
-//        }
-//    }
-//
-//    private void requestStorePermission() {
-//        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
-//            new AlertDialog.Builder(this)
-//                    .setTitle("Permission Denied")
-//                    .setMessage("Media & Storage" + "Allowing Motivation Quotes to access your gallery and files permission you to store images")
-//                    .setPositiveButton("Give Permission", new DialogInterface.OnClickListener() {
-//                        @Override
-//                        public void onClick(DialogInterface dialog, int which) {
-//                            ActivityCompat.requestPermissions(HomeActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
-//
-//                        }
-//                    })
-//                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-//                        @Override
-//                        public void onClick(DialogInterface dialog, int which) {
-//                            UIHelper.showLongToastInCenter(HomeActivity.this, "The Permission was denied");
-//                            finish();
-//                        }
-//                    }).create().show();
-//
-//        } else {
-//            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_WRITE_PERMISSION);
-//        }
-//    }
-//
-//    @Override
-//    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-//        if (requestCode == REQUEST_WRITE_PERMISSION) {
-//            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//                Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show();
-//                loadHomeFragment();
-//            } else {
-//                Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show();
-//                finish();
-//            }
-//        }
-//    }
 }
--- AdvanceCallVideoRecorder/app/src/main/java/com/bilalzaman/advancecallvideorecorder/fragments/SettingsFragment.java
+++ AdvanceCallVideoRecorder/app/src/main/java/com/bilalzaman/advancecallvideorecorder/fragments/SettingsFragment.java
@@ -2,35 +2,51 @@ package com.bilalzaman.advancecallvideorecorder.fragments;
 
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.CompoundButton;
 import android.widget.ImageButton;
-import android.widget.Switch;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.bilalzaman.advancecallvideorecorder.R;
-import com.bilalzaman.advancecallvideorecorder.activities.StorageActivity;
-import com.bilalzaman.advancecallvideorecorder.activities.MoreActivity;
+import com.bilalzaman.advancecallvideorecorder.activities.LockScreenActivity;
 import com.bilalzaman.advancecallvideorecorder.activities.NotificationActivity;
+import com.bilalzaman.advancecallvideorecorder.activities.StorageActivity;
+import com.bilalzaman.advancecallvideorecorder.dialogs.RecordDialog;
 import com.bilalzaman.advancecallvideorecorder.helpers.UIHelper;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import butterknife.Unbinder;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SettingsFragment extends Fragment implements View.OnClickListener {
+public class SettingsFragment extends Fragment {
 
-    private ConstraintLayout recordLayout, notificationLayout, cloudLayout, moreLayout, proLayout, shareLayout, rateLayout;
+    Unbinder unbinder;
     private Context context;
-    private Switch switchRecord;
     private TextView txtTitle;
-    private ImageButton imgBack;
+    //    @BindView(R.id.txtRecord)
+//    TextView txtRecord;
+//    @BindView(R.id.txtNotification)
+//    TextView txtNotification;
+//    @BindView(R.id.txtCloud)
+//    TextView txtCloud;
+//    @BindView(R.id.txtStorage)
+//    TextView txtStorage;
+//    @BindView(R.id.txtLock)
+//    TextView txtSecurity;
+//    @BindView(R.id.txtPro)
+//    TextView txtPro;
+//    @BindView(R.id.txtShare)
+//    TextView txtShare;
+//    @BindView(R.id.txtRate)
+//    TextView txtRate;
+    ImageButton imgBack;
 
     public SettingsFragment() {
         // Required empty public constructor
@@ -42,72 +58,69 @@ public class SettingsFragment extends Fragment implements View.OnClickListener {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
         context = container.getContext();
+        unbinder = ButterKnife.bind(this, view);
         txtTitle = view.findViewById(R.id.txtTitle);
         imgBack = view.findViewById(R.id.imgBack);
-
         txtTitle.setText("Setting");
         txtTitle.setVisibility(View.VISIBLE);
         imgBack.setVisibility(View.GONE);
-        recordLayout = view.findViewById(R.id.recorder_layout);
-        notificationLayout = view.findViewById(R.id.notification_layout);
-        cloudLayout = view.findViewById(R.id.cloud_layout);
-        moreLayout = view.findViewById(R.id.more_layout);
-        proLayout = view.findViewById(R.id.pRO_layout);
-        shareLayout = view.findViewById(R.id.share_layout);
-        rateLayout = view.findViewById(R.id.rate_layout);
-        switchRecord = view.findViewById(R.id.switchRecord);
-
-        recordLayout.setOnClickListener(this);
-        notificationLayout.setOnClickListener(this);
-        cloudLayout.setOnClickListener(this);
-        moreLayout.setOnClickListener(this);
-        proLayout.setOnClickListener(this);
-        shareLayout.setOnClickListener(this);
-        rateLayout.setOnClickListener(this);
-
-        switchRecord.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                Toast.makeText(context, "is on", Toast.LENGTH_SHORT).show();
-
-            }
-        });
 
         return view;
+
+    }
+
+    @OnClick({R.id.txtRecord, R.id.record_desc})
+    void record() {
+        RecordDialog recordFragment = new RecordDialog();
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_container, recordFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+    }
+
+    @OnClick({R.id.txtNotification, R.id.notification_desc})
+    void notification() {
+        UIHelper.openActivity(getActivity(), NotificationActivity.class);
+
+    }
+
+    @OnClick(R.id.txtCloud)
+    void cloudAccount() {
+
+    }
+
+    @OnClick(R.id.txtStorage)
+    void storage() {
+        UIHelper.openActivity(getActivity(), StorageActivity.class);
+    }
+
+
+    @OnClick(R.id.txtLock)
+    void security() {
+        UIHelper.openActivity(getActivity(), LockScreenActivity.class);
+    }
+
+
+    @OnClick(R.id.txtPro)
+    void videoPro() {
+
+    }
+
+    @OnClick(R.id.txtShare)
+    void shareApplication() {
+
+    }
+
+    @OnClick(R.id.txtRate)
+    void rate() {
+
     }
 
     @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.notification_layout: {
-                UIHelper.openActivity(getActivity(), NotificationActivity.class);
-                break;
-            }
-            case R.id.cloud_layout: {
-                UIHelper.openActivity(getActivity(), StorageActivity.class);
-                break;
-            }
-            case R.id.more_layout: {
-                UIHelper.openActivity(getActivity(), MoreActivity.class);
-                break;
-            }
-            case R.id.pRO_layout: {
+    public void onDestroyView() {
+        super.onDestroyView();
 
-                break;
-            }
-            case R.id.share_layout: {
-                Intent shareIntent = new Intent(android.content.Intent.ACTION_SEND);
-                shareIntent.setType("text/plain");
-                shareIntent.putExtra(Intent.EXTRA_SUBJECT, "");
-                shareIntent.putExtra(Intent.EXTRA_TEXT, "Check our App on Playstore " +
-                        "google.com");
-                context.startActivity(Intent.createChooser(shareIntent, context.getResources().getString(R.string.share_using)));
-                break;
-            }
-            case R.id.rate_layout: {
-
-                break;
-            }
-        }
+        unbinder.unbind();
     }
+
 }
